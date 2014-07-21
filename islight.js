baabdb5f3ba5e14f5717d2ecf9c04b8dbdedad31
Jacob <jakeboone@aol.com>
2014-07-21 13:38:32 -0600

    completed islight 1.2

--- islight.js
+++ islight.js
@@ -1,12 +1,13 @@
-#!/usr/bin/env node
-
-
-//  Part 1.1------------
+//  Part 2.1------------
 var r = process.argv[2]
 var g = process.argv[3]
 var b = process.argv[4]
 
 var luminosity = 0.2126*r + 0.7152*g + 0.0722*b
 
-console.log(luminosity) 
-
+if(luminosity > 155){
+	console.log('light')
+}
+else{
+	console.log('dark')
+}
